fe4789185dae3158f8265cb1abcbd10442039539
“reddrum” <anton.kranz@me.com>
2017-03-31 22:54:13 +0300

    Added faker contacts

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
-
+gem 'faker', '~> 1.7', '>= 1.7.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.destroy_all
+
+contacts = []
+
+# generate 30 fake data
+30.times do |i|
+  new_contact = {
+      id: i,
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      address: "#{Faker::Address.street_name} #{Faker::Address.zip} #{Faker::Address.city}",
+      phone: Faker::PhoneNumber.cell_phone
+  }
+  contacts.push(new_contact)
+end
+
+# Save the fake data to database
+Contact.create(contacts)
