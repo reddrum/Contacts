5a33aaa2c930c2da1d551dde0a3ef53fdbd98160
“reddrum” <anton.kranz@me.com>
2017-04-01 23:07:13 +0300

    Added form for new contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,24 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Success!"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :address, :phone)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
+  resources :contacts
 
   root 'contacts#index'
 end
